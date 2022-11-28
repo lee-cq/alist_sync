a7d6e3de4c5f9fe304eb4162b9733215d2128acd
Lee CQ <lee_cq@icloud.com>
2022-11-29 00:37:49 +0800

    功能测试1

--- test/t_sync.py
+++ test/t_sync.py
@@ -16,14 +16,12 @@ logging.config.dictConfig(yaml.safe_load(open('logger_config.yml').read()))
 logger = logging.getLogger('alist.test.sync')
 
 
-
 def sync_():
     conf = yaml.safe_load(open('config.yml').read())
     with Sync(conf['sync_group'][0]) as s:
         s.scan_update_file()
+        s.move_old_file()
     print('off')
-    for t in threading.enumerate():
-        print(t.name, t.is_alive(), t.isDaemon())
 
 
 if __name__ == '__main__':
